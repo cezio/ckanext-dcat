ac6be44c6d36f09e112a7a9599346fa26b153e47
Cezary Statkiewicz <cezio@thelirium.net>
2017-10-16 14:36:32 +0200

    tags filtering in rdf profile

--- ckanext/dcat/profiles.py
+++ ckanext/dcat/profiles.py
@@ -4,6 +4,7 @@ import json
 from dateutil.parser import parse as parse_date
 
 from pylons import config
+from paste.deploy.converters import asbool
 
 import rdflib
 from rdflib import URIRef, BNode, Literal
@@ -12,8 +13,9 @@ from rdflib.namespace import Namespace, RDF, XSD, SKOS, RDFS
 from geomet import wkt, InvalidGeoJSONException
 
 from ckan.plugins import toolkit
+from ckan.lib.munge import munge_tag
 
-from ckanext.dcat.utils import resource_uri, publisher_uri_from_dataset_dict
+from ckanext.dcat.utils import resource_uri, publisher_uri_from_dataset_dict, DCAT_CLEAN_TAGS
 
 DCT = Namespace("http://purl.org/dc/terms/")
 DCAT = Namespace("http://www.w3.org/ns/dcat#")
@@ -617,8 +619,16 @@ class EuropeanDCATAPProfile(RDFProfile):
             keywords.remove(keyword)
             keywords.extend([k.strip() for k in keyword.split(',')])
 
+        # replace munge_tag to noop if there's no need to clean tags
+
+        clean_tags = asbool(config.get(DCAT_CLEAN_TAGS, False))
+        def clean_tag(value):
+            if clean_tags:
+                return munge_tag(value)
+            return value
+        
         for keyword in keywords:
-            dataset_dict['tags'].append({'name': keyword})
+            dataset_dict['tags'].append({'name': clean_tag(keyword)})
 
         # Extras
 
